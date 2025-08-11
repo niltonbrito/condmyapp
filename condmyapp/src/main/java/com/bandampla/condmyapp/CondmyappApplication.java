4568517b57f0e7a9eedf56936ea98b4f930efdbc
TI-RBD <suporte.ti@rbdimagem.com.br>
2025-08-11 17:28:13 -0300

    Primeiro commit da aplicação

---
+++ condmyapp/src/main/java/com/bandampla/condmyapp/CondmyappApplication.java
@@ -0,0 +1,13 @@
+package com.bandampla.condmyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondmyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondmyappApplication.class, args);
+	}
+
+}
---
+++ condmyapp/src/test/java/com/bandampla/condmyapp/CondmyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandampla.condmyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondmyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
