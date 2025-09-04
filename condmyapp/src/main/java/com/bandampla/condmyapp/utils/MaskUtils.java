4be45ca77c03137f1edb21ec3a3f7d8c50b2a2b5
Nilton-Brito <nilton.brito@outlook.com>
2025-09-03 21:22:22 -0300

    Commit - Criado a classe MaskUtils para efetuar marcaramento de dados sensíveis como CPF e Celular

---
+++ condmyapp/src/main/java/com/bandampla/condmyapp/utils/MaskUtils.java
@@ -0,0 +1,35 @@
+package com.bandampla.condmyapp.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component("maskUtils")
+public class MaskUtils {
+
+    public static String maskCpf(String cpf) {
+        if (cpf.isEmpty() || cpf.trim().length() != 11) {
+            return cpf; // retorna sem alteração se for inválido
+        }
+        return cpf.substring(0, 3) + "." +
+               cpf.substring(3, 4) + "**.***-**";
+    }
+    
+    public static String maskPhone(String phone) {
+        if (phone == null ||phone.isBlank()) return "";
+
+        String digits = phone.replaceAll("\\D", "");
+
+        if (digits.length() == 10) {
+            return String.format("(%s) %s-%s",
+                    digits.substring(0, 2),
+                    digits.substring(2, 6),
+                    digits.substring(6));
+        } else if (digits.length() == 11) {
+            return String.format("(%s) %s-%s",
+                    digits.substring(0, 2),
+                    digits.substring(2, 7),
+                    digits.substring(7));
+        }
+
+        return phone;
+    }
+}
